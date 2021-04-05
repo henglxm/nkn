9f8536d786241f0f4f6ac0e9f23a2c5ee2f4df45
hengoye <hengoye@sina.cn>
2021-04-05 14:10:02 +0800

    up

---
+++ main.py
@@ -0,0 +1,220 @@
+import boto3
+import json
+import sys
+import time
+import os
+import base64
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
+
+    """
+    access = os.getenv('AWS_ACCESS_KEY')
+    secret = os.getenv('AWS_SECRET_ACCESS')
+    count = int(os.getenv('AWS_LAUNCH_COUNT'))
+    instane_type = os.getenv('INSTANCE_TYPE')
+    region = os.getenv('AWS_REGION')
+    ami = os.getenv('AWS_AMI')
+    nkn_path = os.getenv('ARM_NKN_PATH')
+    param = None
+    try:
+        param = sys.argv[1] if sys.argv[1] else None
+    except Exception as e:
+        pass
+    if param and param == 'getip':
+        get_ip(access=access, secret=secret, region=region)
+        return False
+    if param and param == 'init':
+        # instane_type = os.getenv('INIT_TYPE')
+        # ami = os.getenv('INIT_AMI')
+        # nkn_path = os.getenv('AMD_NKN_PATH')
+        apikey = {
+            'AKIAWYBUD74JRHJEAPNM': 'SNxGLbnlbevPgLtuPq/qIOZJlaos8w9Qz+IxVofq',
+            'AKIA3RKZEPC6D6JKOS6I': 'Y++UarhHA5Vd6VRSqJbeZxq0QIo3wYylNYJfJ7bU',
+            'AKIA56DRF7A7J3IXOPTV': 'c2KSTZaawx9gkUUEcEPFnHtq+pOKIrN9DJ0MyJno',
+            'AKIA3PGLZS3EVPJLHLUU': 'f4t9Uxcu+cbG/vb/Y8yoQ0kGslwsmdYhZaQS4+Ym',
+            'AKIAQDAS6KBD3SPKIGKB': '56aDbKQMPZbAjOG/9QqmlWB3Ukoh/8kn59QHeIEx'
+        }
+        for k in apikey:
+            group_id = create_security_group(access=k,
+                                             secret=apikey[k],
+                                             region=region)
+            instances = create_instances(group_id=group_id,
+                                         access=k,
+                                         secret=apikey[k],
+                                         count=1,
+                                         instane_type=instane_type,
+                                         region=region,
+                                         nkn_path=nkn_path,
+                                         ami=ami)
+
+        return False
+        count = 1
+
+    group_id = create_security_group(access=access,
+                                     secret=secret,
+                                     region=region)
+    print("group_id : " + group_id)
+    instances = create_instances(group_id=group_id,
+                                 access=access,
+                                 secret=secret,
+                                 count=count,
+                                 instane_type=instane_type,
+                                 region=region,
+                                 nkn_path=nkn_path,
+                                 ami=ami)
+    print('==========================')
+    ids = []
+    i = 1
+    for instance in instances:
+        print(instance.id)
+    print('请等待1分钟')
+    time.sleep(60)
+    for instance in instances:
+        client = boto3.client('ec2',
+                              aws_access_key_id=access,
+                              aws_secret_access_key=secret,
+                              region_name=region)
+        desc = client.describe_instances(InstanceIds=[instance.id])
+        for ins in desc['Reservations']:
+            print(
+                str(i) + '   ' + region + '   ' + '   ' +
+                ins['Instances'][0]['InstanceId'] + '   ' +
+                ins['Instances'][0]['PublicIpAddress'] + '   ' +
+                ins['Instances'][0]['InstanceType'] + '   ' +
+                str(ins['Instances'][0]['LaunchTime']))
+            i = i + 1
+
+    print('==========================')
+
+
+def create_security_group(access, secret, region):
+    """TODO: Docstring for create_security_group.
+    :arg1: TODO
+    :returns: TODO
+    """
+    print('创建安全组...')
+    client = boto3.client('ec2',
+                          aws_access_key_id=access,
+                          aws_secret_access_key=secret,
+                          region_name=region)
+    security_group = client.create_security_group(GroupName='nkn' +
+                                                  str(time.time()),
+                                                  Description="nkn")
+    client.authorize_security_group_ingress(CidrIp='0.0.0.0/0',
+                                            IpProtocol='tcp',
+                                            FromPort=1,
+                                            ToPort=65535,
+                                            GroupId=security_group['GroupId'])
+    group_id = security_group['GroupId']
+    print('安全组创建成功, ID ： ' + group_id)
+    return group_id
+
+
+def create_instances(group_id, access, secret, count, instane_type, region,
+                     nkn_path, ami):
+    """TODO: Docstring for create_instance.
+    :returns: TODO
+
+    """
+    print('创建ec2 ... ： ')
+    ec2 = boto3.resource('ec2',
+                         aws_access_key_id=access,
+                         aws_secret_access_key=secret,
+                         region_name=region)
+    instances = ec2.create_instances(ImageId=ami,
+                                     InstanceType=instane_type,
+                                     MaxCount=int(count),
+                                     MinCount=int(count),
+                                     Monitoring={'Enabled': False},
+                                     SecurityGroupIds=[group_id],
+                                     UserData=get_user_data(nkn_path))
+    # InstanceMarketOptions={
+    # 'MarketType': 'spot',
+    # 'SpotOptions': {
+    # 'SpotInstanceType': 'persistent',
+    # 'InstanceInterruptionBehavior':
+    # 'stop'
+    # }
+    # }
+    print('ec2 创建成功')
+    return instances
+
+
+def get_user_data(nkn_path):
+    """TODO: Docstring for get_user_data.
+    :returns: TODO
+
+    """
+    userdata = """#!/bin/bash
+sleep 2m
+sudo -i
+systemctl stop nkn-commercial
+cd %(nkn_path)s
+rm wallet.json
+rm wallet.pswd
+echo 123123 | sudo tee wallet.pswd
+./nknc wallet -c --password=123123
+systemctl start nkn-commercial"""
+
+    userdata = userdata % dict(nkn_path=nkn_path)
+
+    bytestring = userdata.encode(encoding='utf-8')
+    return base64.encodebytes(bytestring)
+
+
+def get_ip(access, secret, region):
+    """TODO: Docstring for get_ip.
+    :returns: TODO
+
+    """
+    f = open("ids.txt", "r+")
+    lines = f.readlines()
+    ids = ""
+    i = 1
+    for line in lines:
+        ids = line.rstrip('\n')
+        client = boto3.client('ec2',
+                              aws_access_key_id=access,
+                              aws_secret_access_key=secret,
+                              region_name=region)
+        desc = client.describe_instances(InstanceIds=[ids])
+        ids = []
+        for instance in desc['Reservations']:
+            print(
+                str(i) + '   ' + region + '   ' + '   ' +
+                instance['Instances'][0]['InstanceId'] + '   ' +
+                instance['Instances'][0]['PublicIpAddress'] + '   ' +
+                instance['Instances'][0]['InstanceType'] + '   ' +
+                str(instance['Instances'][0]['LaunchTime']))
+            i = i + 1
+
+
+if __name__ == "__main__":
+    proxy = os.getenv('PROXY')
+    if proxy is not None:
+        os.environ["HTTP_PROXY"] = proxy
+        os.environ["HTTPS_PROXY"] = proxy
+    for param in sys.argv:
+        if param.find('--') != -1 and param[0:2] == '--' and param.find(
+                '=') != -1:
+            key = param[2:param.find('=')].strip()
+            value = param[param.find('=') + 1:].strip()
+            if key == 'access':
+                access = value
+            if key == 'secret':
+                secret = value
+            if key == 'count':
+                count = value
+            if key == 'type':
+                instane_type = value
+            if key == 'region':
+                region = value
+            if key == 'ami':
+                ami = value
+    main()
